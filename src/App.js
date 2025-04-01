a0b5515248c293d70772f8d56252cfd5d555b052
leela8374 <leelamohankurmapu2004@gmail.com>
2025-04-01 23:04:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
